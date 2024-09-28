8330f77185ab94f1a79e8e6b3409de04a5fb81a2
afjalali786 <afjalali786078@gmail.com>
2024-09-28 22:15:01 +0530

    DAY 1
    level=easy

---
+++ team.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+int main (){
+    int score;
+    printf ("Enter the team's score :");
+    scanf ("%d",&score);
+    if (score >=12){
+        printf("The team has qualified for the next stage.\n");
+    }
+        else {
+            printf ("The team has NOT qualified for the next stage.\n");
+        }
+    return 0;
+}
